9f4179b4b5b8bb60b4d595d2a2a31f0238b7e3e4
PedroNogueir4 <pedroffn209@gmail.com>
2022-10-31 17:36:08 -0300

    updateAll

--- scripts.js
+++ scripts.js
@@ -1,15 +1,48 @@
 const button = document.getElementById('button-convert')
+const select = document.getElementById('options-convert')
+const dolar = 5.17
+const euro = 6
 
-const dolar = 5
-const convertValues= () =>{
+const convertValues = () => {
 
     const inputReal = document.getElementById('value-input').value
+    const number = document.getElementById('real-value')
+    const number2 = document.getElementById('convert-value')
+    const dolarValue = inputReal / dolar
+    const euroValue = inputReal / euro
 
-console.log(inputReal / dolar)
+    number.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: 'BRL' }
+    ).format(inputReal);
+    if (select.value === 'US$ Dolar Americano') {
 
+        number2.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(dolarValue);
+    }
+    if (select.value === ' € Euro') {
+
+        number2.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'EUR' }
+        ).format(euroValue);
+    }
+}
+const exchangeCoins = () => {
+    const coin = document.getElementById('coin')
+    const coinImage = document.getElementById('image-coin')
+    if (select.value === 'US$ Dolar Americano') {
+        coin.innerHTML = 'Dólar Americano'
+        coinImage.src = "./assets/estados-unidos (1) 1.svg"
+    }
+    if (select.value === '€ Euro') {
+        coin.innerHTML = 'Euro'
+        coinImage.src = "./assets/euro.svg"
+    }
+    
 }
 
 
-button.addEventListener('click',convertValues)
 
+button.addEventListener('click', convertValues)
+select.addEventListener('change', exchangeCoins)
 
